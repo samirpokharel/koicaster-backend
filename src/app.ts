17bb54bea22f3117447869b7c291ed4b8cb1a198
Sameer Pokhrel <i.amsameer2061@gmail.com>
2024-09-06 23:44:44 +0545

    Project Configuration - Env,Error Handler, async Handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
 import express, { Express } from "express";
+import errorHandler from "./middleware/error.js";
+import cors, { CorsOptions } from "cors";
+import dotenv from "dotenv";
 
 const app = express();
+
+const CORS_OPTION: CorsOptions = {
+  origin:  "http://localhost:3000" , // TODO: check for NODE_ENV === "development" leter 
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true,
+};
+
+dotenv.config();
+app.use(cors(CORS_OPTION));
+
+
+app.use(errorHandler);
 
 export default app;
